a0680f331d5efd8660be47a40f909dc6bce918e7
RowanAshour <rokamino@hotmail.com>
2024-01-02 11:11:33 -0800

    2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 # Print the ASCII alphabet in lowercase without a new line
-print("".join("{:c}".format(char) for char in range(ord('a'), ord('z') + 1)), end='')
+print("".join("{:c}".format(char) for char in range(ord('a'), ord('z') + 1)),
+      end='')
