f57f64e9131a84bcd00498d67d8d30ad6ba12bdb
RowanAshour <rokamino@hotmail.com>
2024-01-02 12:35:29 -0800

    10

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """
+    Add two integers and return the result.
+
+    Args:
+    - a, b: integers
+
+    Returns:
+    - The value of a + b
+    """
+    return a + b
+
+
+# Test the function
+if __name__ == "__main__":
+    print(add(1, 2))
+    print(add(98, 0))
+    print(add(100, -2))
