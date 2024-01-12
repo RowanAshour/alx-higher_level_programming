222f05d7867b429f159265d016bc2729802559b6
RowanAshour <rokamino@hotmail.com>
2024-01-12 11:51:27 -0800

    1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    # Create a new list by iterating over the original list
+    # Replace occurrences of 'search' with 'replace'
+    new_list = [replace if item == search else item for item in my_list]
+    
+
+    return new_list
